8eee573b455bdeea6277925f07bfe9df78272f60
Hamaz-Chihab <c_hamaz@estin.dz>
2023-10-14 18:37:50 +0100

    the finale touch

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,9 @@
-const jwt = require('jsonwebtoken');//to compare the token we get from the API call
-const User = require('../models/User.js');//to check the ID avaliable in the token
+const User =require('../models/User.js');//to check the ID avaliable in the token
 
 const auth = async(req ,res, next)=>{
+    const jwt =require('jsonwebtoken');//to compare the token we get from the API call
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        // const token = req.header('Authorization').replace('Bearer ', '');
+        const token = req.header('Authorization').replace('Bearer ', '');//if you have a probleme try  // const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const user = await User.findOne({//extract the ID from decoded-object 
             _id: decoded._id,
